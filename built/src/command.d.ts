5046afcc8e5e169dde4a65ee4578531aa9c2e127
Nick Grayson <intercomman14@gmail.com>
2022-11-17 15:52:40 -0800

    added cursory support for args

--- built/src/command.d.ts
+++ built/src/command.d.ts
@@ -1,7 +1,7 @@
 interface command {
     name: string;
     aliases?: Array<string>;
-    callback: () => Promise<any>;
+    callback: (args: Array<any>) => Promise<any>;
     description?: string;
     minArgs?: number;
     maxArgs?: number;
